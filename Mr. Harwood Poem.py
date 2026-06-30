ee5d4a85f71494d7f4c4a9c5aa8c97056236681c
Henners <mitsywookiefeet@outlook.com>
2026-06-30 22:13:57 +0100

    Add files via upload

---
+++ Mr. Harwood Poem.py
@@ -0,0 +1,35 @@
+import time
+
+for i in "The Code of Mr. Harwood\n":
+    print(i,end = "",flush=True)
+    time.sleep(0.06)
+
+time.sleep(1)
+
+for i in "\nIn the room where the monitors brightly glow,\nSits Peter Harwood, the master we know.\nFor the CS gang of 2026, he’s the guide,\nLeading our crew through the tech countryside.\n":
+    print(i, end = "", flush = True)
+    time.sleep(0.06)
+    
+time.sleep(0.75)
+
+for i in "\nHe walks us through Python, indent by indent,\nTracing where MAR and the MDR went.\nBut don't ask us what a structured diagram means—\nThat concept is completely blank on our screens!\nAnd those actual exams? Well, the pass rate is low,\nBut we still love the class and we enjoy the show.\n":
+    print(i,end = "",flush=True)
+    time.sleep(0.06)
+
+time.sleep(0.75)
+
+for i in "\nThe past is the past, we have left it behind,\nWith just a small mention of Chandler in mind.\nNow Peter keeps driving the darkness away,\nWhile the \"Mango\" Teams chat pings throughout the day.\nSome talk about hacking into the mainframe,\nWhile an instant reaction is called out by name.\nA familiar voice shouts right where we sat:\n":
+    print(i,end = "",flush=True)
+    time.sleep(0.06)
+
+time.sleep(0.75)
+
+for i in "\n\"I'm not going to IR for that!\"\n":
+    print(i,end = "",flush=True)
+    time.sleep(0.06)
+
+time.sleep(0.75)
+
+for i in "\nSo here’s to our mentor who keeps it all bright,\nFrom compile-time errors to scripts running right.\nWith Python and hardware, he leads the whole band,\nPeter Harwood's the best tech boss in the land.\n":
+    print(i,end = "",flush=True)
+    time.sleep(0.06)
